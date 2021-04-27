466f5de4b5764f8d7324eb40eb2aa9424cfe70a1
Leonardo Pimentel <49254413+PimLeonardo@users.noreply.github.com>
2021-04-27 17:52:47 -0300

    Add files via upload

---
+++ Union/nav.php
@@ -0,0 +1,11 @@
+<?php
+
+?>
+
+<div class="navegar">
+	<img src="img/logo.png" width="8%">
+	<a href="index.php">Inicio</a>
+	<a href="informacoes.php">Informações</a>
+	<a href="blog.php">Blog</a>
+	<a href="contato.php">Contato</a>
+</div>
