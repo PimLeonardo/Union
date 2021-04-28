63ede1e3401f99e9d26236603620f7779c953bd4
Leonardo Pimentel <49254413+PimLeonardo@users.noreply.github.com>
2021-04-28 16:55:02 -0300

    Add files via upload

--- Union/index.php
+++ Union/index.php
@@ -5,19 +5,16 @@
 <html>
 	<head>
 		<title>Union</title>
+
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
 		<?php include("nav.php");?>
-		<center>
-			<div class="menu">
-				<div class="titulo">
-					<h1>titulo</h1>
-				</div>
-				<div class="texto">
-					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-				</div>
-			</div>
-		</center>
+
 	</body>
 </html>
--- Union/nav.php
+++ Union/nav.php
@@ -2,10 +2,34 @@
 
 ?>
 
-<div class="navegar">
-	<img src="img/logo.png" width="8%">
-	<a href="index.php">Inicio</a>
-	<a href="informacoes.php">Informações</a>
-	<a href="blog.php">Blog</a>
-	<a href="contato.php">Contato</a>
-</div>
+<header>
+	<nav class="navbar navbar-expand-sm navbar-light bg-success">
+		<div class="container">
+            <img src="img/logo.png" width="150">
+         	
+          	<button class="navbar-toggler" data-toggle="collapse" data-target="#nav-principal">
+            	<span class="navbar-toggler-icon"> </span>
+          	</button>
+
+          	<div class="collapse navbar-collapse" id="nav-principal">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a href="index.php" class="nav-link">Inicio</a>
+					</li>
+					<li class="nav-item">
+						<a href="sobre.php" class="nav-link">Sobre</a>
+					</li>
+					<li class="nav-item">
+						<a href="informacoes.php" class="nav-link">Informações</a>
+					</li>
+					<li class="nav-item">
+						<a href="blog.php" class="nav-link">Blog</a>
+					</li>
+					<li class="nav-item">
+						<a href="contato.php" class="nav-link">Contato</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</header>
